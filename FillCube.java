18c45db09be65da14e47ff6d0299068d2479c004
12307130083@fudan.edu.cn <zhengzhedong@zhengzhongdeMBP.lan>
2015-12-01 21:22:19 +0800

    add thread

--- FillCube.java
+++ FillCube.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class FillCube implements KeyListener {
 	
@@ -11,7 +12,35 @@ public class FillCube implements KeyListener {
 	public FillCube() {
 		start();
 	}
-	
+    
+    class ThreadX implements Runnable{
+        public void run(){
+            centerPanel.clear();
+            centerPanel.CubeRotateX();
+            centerPanel.draw();
+        }
+    }
+    
+    class ThreadY implements Runnable{
+        public void run(){
+            centerPanel.clear();
+            centerPanel.CubeRotateY();
+            centerPanel.draw();
+        }
+    }
+    
+    class ThreadZ implements Runnable{
+        public void run(){
+            centerPanel.clear();
+            centerPanel.CubeRotateZ();
+            centerPanel.draw();
+        }
+    }
+    
+    public ScheduledExecutorService t1 = Executors.newScheduledThreadPool(1);
+    public ScheduledExecutorService t2 = Executors.newScheduledThreadPool(2);
+    public ScheduledExecutorService t3 = Executors.newScheduledThreadPool(3);
+    
 	public void start() {
 		mainFrame = new JFrame("Reconstruct Cube");
 		int width = Cons.mainWidth;
@@ -45,24 +74,30 @@ public class FillCube implements KeyListener {
         if (e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') {
             System.exit(0);
         }
-		if (e.getKeyChar() == 'x' || e.getKeyChar() == 'X') {
-            centerPanel.clear();
-            centerPanel.CubeRotateX();
-            centerPanel.draw();
+        if (e.getKeyChar() == 'x' || e.getKeyChar() == 'X') {
+            t1 = Executors.newScheduledThreadPool(1);
+            t1.scheduleAtFixedRate(new ThreadX(),0,300,TimeUnit.MILLISECONDS);
 		}
         if (e.getKeyChar() == 'y' || e.getKeyChar() == 'Y') {
-            centerPanel.clear();
-            centerPanel.CubeRotateY();
-            centerPanel.draw();
+            t2 = Executors.newScheduledThreadPool(2);
+            t2.scheduleAtFixedRate(new ThreadY(),0,300,TimeUnit.MILLISECONDS);
         }
         if (e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
-            centerPanel.clear();
-            centerPanel.CubeRotateZ();
-            centerPanel.draw();
+            t3 = Executors.newScheduledThreadPool(3);
+            t3.scheduleAtFixedRate(new ThreadZ(),0,300,TimeUnit.MILLISECONDS);
         }
     }
 
 	public void keyReleased(KeyEvent e) {
+        if (e.getKeyChar() == 'x' || e.getKeyChar() == 'X') {
+            t1.shutdown();
+        }
+        if (e.getKeyChar() == 'y' || e.getKeyChar() == 'Y') {
+            t2.shutdown();
+        }
+        if (e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
+            t3.shutdown();
+        }
     }
 
 	public void keyTyped(KeyEvent e) {
